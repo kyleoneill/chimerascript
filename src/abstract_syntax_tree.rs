8aae9dcaa96c0c06abdd286f7e560294acce02c1
kyle o'neill <kyleoneill24@gmail.com>
2023-09-23 12:22:41 -0400

    Tiny commit adding some structure to token parsing

--- src/abstract_syntax_tree.rs
+++ src/abstract_syntax_tree.rs
@@ -1,4 +1,4 @@
-use pest::iterators::Pairs;
+use pest::iterators::{Pair, Pairs};
 use crate::err_handle::ChimeraError;
 use crate::frontend::Rule;
 
@@ -8,17 +8,32 @@ pub struct ChimeraScriptAST {
 }
 
 impl ChimeraScriptAST {
-    /// Convert
+    /// Convert Pest tokens into an abstract syntax tree.
     pub fn from_pairs(pairs: Pairs<Rule>) -> Result<Self, ChimeraError> {
-        println!("{:?}", pairs);
+        // This might be hacky but every valid Pairs should be a list of a single Pair with a Statement rule
+        // We want to discard the outermost Pair and begin parsing on what the Statement contains
+        // TODO: Verify that the outermost Pair is a Statement before discarding it like this.
+        //       I don't think it's possible to not be a statement but good error handling is good
+        for pair in pairs {
+            let inner = pair.into_inner();
+            let statement = ChimeraScriptAST::parse_value(inner.peek().unwrap())?;
+            return Ok(ChimeraScriptAST { statement })
+        }
         Err(ChimeraError::FailedParseAST("Failed to convert pest tokens to an AST.".to_owned()))
+    }
+
+    fn parse_value(pair: Pair<Rule>) -> Result<Statement, ChimeraError> {
+        match pair.as_rule() {
+            Rule::Statement => Err(ChimeraError::FailedParseAST("Should not have gotten a statement Rule enum while parsing values".to_owned())),
+            _ => { Err(ChimeraError::FailedParseAST("NEED TO FINISH PARSE_VALUES".to_owned())) }
+        }
     }
 }
 
 #[derive(Debug)]
 enum Statement {
     AssignmentExpr(AssignmentExpr),
-    AssertCommand,
+    AssertCommand(AssertCommand),
     PrintCommand(LiteralOrVariable),
     Expression(Expression)
 }
